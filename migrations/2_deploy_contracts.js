87137ff743b7a23a60d81e76c5f648c4ca8f919c
Alex Korotkov <programmerweb@yandex.ru>
2017-12-24 20:41:37 +0300

    delete tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,7 @@
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-var SendEtherToAccaunt = artifacts.require("./SendEtherToAccaunt.sol");
-var AddRequest = artifacts.require("./AddRequest.sol");
 var SendEther = artifacts.require("./SendEther.sol");
 var Donation = artifacts.require("./Donation.sol");
 
 module.exports = function(deployer) {
-  // deployer.link(MetaCoin, SendEtherToAccaunt);
-  deployer.deploy(MetaCoin);
-  deployer.deploy(SendEtherToAccaunt);
-  deployer.deploy(AddRequest);
   deployer.deploy(SendEther);
   deployer.deploy(Donation);
 
